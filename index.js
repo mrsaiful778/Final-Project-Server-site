d0cbd578e1a93735aaa685f05396f15fad8114d7
mrsaiful778 <mrsaifulislam778@gmail.com>
2023-11-18 13:03:19 +0600

    last add

--- index.js
+++ index.js
@@ -1,49 +1,82 @@
-    const express = require('express');
-    const cors = require('cors');
-    const { MongoClient, ServerApiVersion } = require('mongodb');
-    const app = express();
-    const port = process.env.PORT || 5000;
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
 
-    //MIDDLEWARE
-    app.use(cors());
-    app.use(express.json());
-
-
-    //Final-Project-Server_Site1
-    //UJpP4P2DFP9fTrVG
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
 
 
- 
-    const uri = "mongodb+srv://Final-Project-Server_Site1:UJpP4P2DFP9fTrVG@cluster0.zvc5ptn.mongodb.net/?retryWrites=true&w=majority";
 
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri, {
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zvc5ptn.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
     }
-    });
+});
 
-    async function run() {
+async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+        const menuCollection = client.db('bistroDB').collection('menu');
+        const reviewCollection = client.db('bistroDB').collection('reviews');
+
+        app.get('/menu', async(req, res) =>{
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/reviews', async(req, res) =>{
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
-    }
-    run().catch(console.dir);
+}
+run().catch(console.dir);
 
-    app.get('/', (req, res) => {
-    res.send('server is running')
-    })
+app.get('/', (req, res) => {
+    res.send('Final project part-1 server is running')
+})
 
 
-    app.listen(port, () => {
+app.listen(port, () => {
     console.log(`server is running on port: ${port}`)
-    });     
+});     
