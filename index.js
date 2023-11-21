8e52063c0b308548e68751e5155703f2e29e044d
mrsaiful778 <mrsaifulislam778@gmail.com>
2023-11-22 00:59:15 +0600

    food add to the database

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
 
         const menuCollection = client.db('bistroDB').collection('menu');
         const reviewCollection = client.db('bistroDB').collection('reviews');
+        const cartCollection = client.db('bistroDB').collection('carts');
 
         app.get('/menu', async(req, res) =>{
             const result = await menuCollection.find().toArray();
@@ -41,7 +42,12 @@ async function run() {
             res.send(result);
         })
 
-
+        //carts collection
+        app.post('/carts', async ( req, res) => {
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
+            res.send(result)
+        })
 
 
 
